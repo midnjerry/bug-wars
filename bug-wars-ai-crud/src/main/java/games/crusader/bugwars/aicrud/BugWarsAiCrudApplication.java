e2f4543fb1b2e5df02ce16afb1725836ecd9f08d
Jerry Balderas <Gerardo.Balderas@cognizant.com>
2020-02-21 19:24:09 -0600

    Add Eureka Client annotation

--- bug-wars-ai-crud/src/main/java/games/crusader/bugwars/aicrud/BugWarsAiCrudApplication.java
+++ bug-wars-ai-crud/src/main/java/games/crusader/bugwars/aicrud/BugWarsAiCrudApplication.java
@@ -2,7 +2,9 @@ package games.crusader.bugwars.aicrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class BugWarsAiCrudApplication {
 	public static void main(String[] args) {
